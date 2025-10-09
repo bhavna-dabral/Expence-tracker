47947bcdec36cf8fbb89b6275218df7c4ff11360
bhavna-dabral <bhavnadabral00@gmail.com>
2025-10-09 23:57:03 +0530

    Add frontend folder as part of main repo

---
+++ backend/db/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+export const db = async () => {
+    try {
+        mongoose.set('strictQuery', false)
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('Db Connected')
+    } catch (error) {
+        console.log('DB Connection Error');
+    }
+}
