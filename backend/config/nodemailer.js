f3cc89718ca2421d626a610267ec8fbfca37de1b
bhavna-dabral <bhavnadabral00@gmail.com>
2025-10-31 19:50:45 +0530

    Ready to deploy

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -6,17 +6,12 @@ dotenv.config();
 
 console.log("SMTP_USER:", process.env.SMTP_USER);
 console.log("SMTP_PASS:", process.env.SMTP_PASS ? "Loaded" : "Missing");
-
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587, // TLS port (STARTTLS)
-  secure: false, // true for 465, false for 587
+  host: "smtp.resend.com",
+  port: 587,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false, // ignore self-signed cert errors in dev
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
   },
 });
 
