80340b8b883a47e36ad06e013bb45995ef56b3c7
Bhavna <bhavnadabral00@gmail.com>
2025-11-10 15:46:12 +0530

    Remove unused import of dollar icon

--- frontend/src/Components/Transactions/Transactions.js
+++ frontend/src/Components/Transactions/Transactions.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { useGlobalContext } from "../../context/globalContext";
 import { InnerLayout } from "../../styles/Layouts";
-import { dollar } from "../../utils/Icons";
+
 
 function Transactions() {
   const { getIncomes, getExpenses, incomes, expenses } = useGlobalContext();
