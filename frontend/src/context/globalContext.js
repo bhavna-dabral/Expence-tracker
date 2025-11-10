3e53c370c8093d7d0bf3df003a563f4e08e386c2
Bhavna <bhavnadabral00@gmail.com>
2025-11-10 15:17:29 +0530

    Fix duplicate declaration of BASE_URL

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,8 +1,7 @@
 import React, { useContext, useState, useCallback } from "react";
 import axios from "axios";
 
-const BASE_URL =
-  const BASE_URL = process.env.REACT_APP_API_URL || "https://expence-tracker2.onrender.com/api/v1/";
+const BASE_URL = process.env.REACT_APP_API_URL || "https://expence-tracker2.onrender.com/api/v1/";
 
 const GlobalContext = React.createContext();
 
